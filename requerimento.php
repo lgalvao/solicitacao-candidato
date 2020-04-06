1af61dab3cc18d354bdafd686bd4063129b891e1
unknown <gimenesjhonathan@gmail.com>
2020-04-06 11:12:44 -0300

    Correção do documento de formatação para iso-8859-1

--- requerimento.php
+++ requerimento.php
@@ -1,9 +1,9 @@
 <?php
 $conteudoRequerimento = "";
 
-$conteudoRequerimento .= "<p>".$nome.", portador do título eleitoral nº ".$titulo.", devidamente qualificado no formulário de pré-atendimento ".$tituloNet.", solicito o processamento do pedido.</p>";
+$conteudoRequerimento .= "<p>".utf8_encode($nome).", portador do título eleitoral nº ".$titulo.", devidamente qualificado no formulário de pré-atendimento ".$tituloNet.", solicito o processamento do pedido.</p>";
 $conteudoRequerimento .= "<p>Para tanto, junto os documentos necessários para comprovar o alegado.</p>";
 $conteudoRequerimento .= "<p>Outrossim, informa que se trata de demanda urgente.</p>";
 $conteudoRequerimento .= "<p>Pede e espera deferimento.</p>";
-$conteudoRequerimento .= "<p>".getAmbiente()["zonaDescricao"].", ".date("d/m/Y H:i:s").".</p>";
+$conteudoRequerimento .= "<p>".utf8_encode(getAmbiente()["zonaDescricao"]).", ".date("d/m/Y H:i:s").".</p>";
 ?>e
